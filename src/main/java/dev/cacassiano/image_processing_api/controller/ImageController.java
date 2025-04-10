9fd8dcc120fb527ccdec6b49ee941b74e9919901
Cassiano <saurinho.64.ss@gmail.com>
2025-04-10 14:25:32 -0300

    update mirror service and controller

--- src/main/java/dev/cacassiano/image_processing_api/controller/ImageController.java
+++ src/main/java/dev/cacassiano/image_processing_api/controller/ImageController.java
@@ -1,7 +1,6 @@
 package dev.cacassiano.image_processing_api.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
@@ -10,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import dev.cacassiano.image_processing_api.service.ImageService;
 
@@ -19,10 +19,8 @@ public class ImageController {
     @Autowired
     private ImageService service;
 
-    @PostMapping(value = "/mirror", consumes = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<byte[]> mirrorEndpoint(InputStream req) throws IOException{
-        return ResponseEntity.ok()
-            .contentType(MediaType.IMAGE_PNG)
-            .body(service.mirrorImage(ImageIO.read(req)));
+    @PostMapping(value = "/mirror", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<byte[]> mirrorEndpoint(MultipartFile image, String format) throws IOException {
+        return service.mirrorImage(ImageIO.read(image.getInputStream()), format);
     }
 }
--- src/main/java/dev/cacassiano/image_processing_api/service/ImageService.java
+++ src/main/java/dev/cacassiano/image_processing_api/service/ImageService.java
@@ -8,21 +8,48 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImageService {
 
-    public byte[] mirrorImage(BufferedImage originalIOImage) throws IOException {
+    public ResponseEntity<byte[]> mirrorImage(BufferedImage originalIOImage, String format) throws IOException {
+        
         AffineTransform transform = AffineTransform.getScaleInstance(-1, 1);
         transform.translate(-originalIOImage.getWidth(), 0);
 
-        AffineTransformOp operation = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+        AffineTransformOp operation = new AffineTransformOp(transform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
         BufferedImage newImage = operation.filter(originalIOImage, null);
 
-        ByteArrayOutputStream armazenador = new ByteArrayOutputStream();
-        ImageIO.write(newImage, "png", armazenador);
-        return armazenador.toByteArray();
+        ByteArrayOutputStream storage = new ByteArrayOutputStream();
+        if(format.equals("png")){
+
+            boolean create = ImageIO.write(newImage, "png", storage);
+            if (create) {
+                return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(storage.toByteArray());
+            } else {
+                return ResponseEntity.unprocessableEntity().build();
+            }
+
+        } else if(format.equals("jpeg")){
+
+            boolean create = ImageIO.write(newImage, "jpeg", storage);
+            if (create) {
+                return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(storage.toByteArray());
+            } else {
+                return ResponseEntity.unprocessableEntity().build();
+            }
+
+        } else {
+            return ResponseEntity.internalServerError()
+                .body(storage.toByteArray());
+        }
          
     }
     
